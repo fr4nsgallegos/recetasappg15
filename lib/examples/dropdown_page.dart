8f2b992d6a5a2e7d827e28aa2047b328d5d8b992
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2026-05-14 22:55:31 -0500

    personalizando dropdownbutton

---
+++ lib/examples/dropdown_page.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+class DropdownPage extends StatefulWidget {
+  @override
+  State<DropdownPage> createState() => _DropdownPageState();
+}
+
+class _DropdownPageState extends State<DropdownPage> {
+  // DropdownButton es un widget que muestra un botón con una opción seleccionada, al tocarlo se despliega una lsita de opciones a escoger
+
+  String? selectedValue;
+  List<String> paises = ["Perú", "Chile", "Argentina", "Brasil"];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Dropdown Page")),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Card(
+              color: Colors.blueGrey,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 32,
+                  vertical: 8,
+                ),
+                child: DropdownButton(
+                  value: selectedValue,
+                  icon: Icon(Icons.arrow_downward, color: Colors.white),
+                  dropdownColor: Colors.green[40], //color del menu desplegable
+                  underline: Container(), //elimina la linea inferior
+                  borderRadius: BorderRadius.circular(
+                    25,
+                  ), //se aplica al desplegable
+                  hint: Text(
+                    "Selecciona el país",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  elevation: 15,
+                  style: TextStyle(
+                    color: Colors.red,
+                    fontSize: 20,
+                  ), //estilo del texto
+                  // focusColor: Colors.white,
+                  // iconEnabledColor: Colors.red,
+                  // iconDisabledColor: Colors.blue,
+                  items: paises.map((pais) {
+                    return DropdownMenuItem(child: Text(pais), value: pais);
+                  }).toList(),
+                  onChanged: (newValue) {
+                    selectedValue = newValue!;
+                    print(newValue);
+                    setState(() {});
+                  },
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:recetasappg15/examples/dropdown_page.dart';
 import 'package:recetasappg15/examples/listview_page.dart';
 import 'package:recetasappg15/examples/scroll_page.dart';
 import 'package:recetasappg15/examples/textfield_page.dart';
@@ -6,5 +7,5 @@ import 'package:recetasappg15/examples/textformfield_page.dart';
 import 'package:recetasappg15/pages/home_page.dart';
 
 void main() {
-  runApp(MaterialApp(home: HomePage(), debugShowCheckedModeBanner: false));
+  runApp(MaterialApp(home: DropdownPage(), debugShowCheckedModeBanner: false));
 }
