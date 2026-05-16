c433edbfaecfd2b0e101b83604c5c61a5838506e
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2026-05-15 22:51:08 -0500

    pasando información entre patanllas con navegación

--- lib/examples/navigator_page.dart
+++ lib/examples/navigator_page.dart
@@ -1,5 +1,21 @@
 import 'package:flutter/material.dart';
 
+// class HomeNavigatorPage extends StatelessWidget {
+//   const HomeNavigatorPage({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(title: Text("Home Navigator PAGE")),
+//       body: Center(
+//         child: Column(mainAxisSize: MainAxisSize.min, children: [
+
+//         ]),
+//       ),
+//     );
+//   }
+// }
+
 class NavigatorPage extends StatelessWidget {
   const NavigatorPage({super.key});
 
@@ -12,11 +28,20 @@ class NavigatorPage extends StatelessWidget {
           mainAxisSize: MainAxisSize.min,
           children: [
             ElevatedButton(
-              onPressed: () {
-                Navigator.push(
+              onPressed: () async {
+                final result = await Navigator.push(
                   context,
-                  MaterialPageRoute(builder: (context) => DetallePage()),
+                  MaterialPageRoute(
+                    builder: (context) => DetallePage(nombrePersona: "jhonny"),
+                  ),
                 );
+
+                // Al volver de detalle page, mostramos el resultado si es que se devuelve algo
+                if (context.mounted && result != null) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(content: Text("Resultado: $result ")),
+                  );
+                }
               },
               child: Text("Ir a la página detalle"),
             ),
@@ -25,7 +50,9 @@ class NavigatorPage extends StatelessWidget {
               onPressed: () {
                 Navigator.pushReplacement(
                   context,
-                  MaterialPageRoute(builder: (context) => DetallePage()),
+                  MaterialPageRoute(
+                    builder: (context) => DetallePage(nombrePersona: "María"),
+                  ),
                 );
               },
               child: Text("ir a detalle sin retorno"),
@@ -38,22 +65,36 @@ class NavigatorPage extends StatelessWidget {
 }
 
 class DetallePage extends StatelessWidget {
-  const DetallePage({super.key});
+  String nombrePersona;
+
+  DetallePage({required this.nombrePersona});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Detalle")),
+      appBar: AppBar(
+        title: Text("Detalle"),
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context, "OK DESDE EL BOTTON APPBAR");
+          },
+          icon: Icon(Icons.arrow_back_ios_new_rounded),
+        ),
+      ),
 
       body: Center(
         child: Column(
           mainAxisSize: MainAxisSize.min,
           children: [
             Text("Soy la página detalle"),
+            Text(
+              "el nombre es $nombrePersona",
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+            ),
             SizedBox(height: 32),
             ElevatedButton(
               onPressed: () {
-                Navigator.pop(context);
+                Navigator.pop(context, "OK DESDE EL DETALLE PAGE");
               },
               child: Text("Retroceder a la pantalla anterior"),
             ),
