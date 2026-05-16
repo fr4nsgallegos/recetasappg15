7ac9f4d08fbb7af96342e50e5535aa0086e09127
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2026-05-15 20:00:17 -0500

    sliverlist

--- lib/examples/sliver/sliver_page.dart
+++ lib/examples/sliver/sliver_page.dart
@@ -23,6 +23,13 @@ class SliverPage extends StatelessWidget {
           // fLUTTER lo define como un appbar de material design que se puede trabajar con CustomscrolllvIEW
           SliverAppBar(
             title: Text("Mi pantalla"),
+            // pinned:
+            // true, //hace que el Appbar quede pegado arriba si hacemos scroll
+            floating:
+                true, //hace que el appbar aparezca rápidamente cando el usuario empieza a hacer scroll hacia arriba
+
+            snap:
+                true, //hace que el appbar aparezca de forma más inmediata y animada (generalmente se usaro con floatin:true)
             expandedHeight: 220, //altura máxima cuando el appbar esta extendido
             // flexibleSpace -> Permite crear un espacio flexible dentro del Appbar
             flexibleSpace: FlexibleSpaceBar(
@@ -33,13 +40,41 @@ class SliverPage extends StatelessWidget {
               ),
             ),
           ),
+          // SliverList -> es una lista que trabaja dentro de CustomScrollView
           SliverList(
-            delegate: SliverChildBuilderDelegate(childCount: 20, (
+            delegate: SliverChildBuilderDelegate(childCount: 25, (
               context,
               index,
             ) {
-              return ListTile(title: Text("Elemento $index"));
+              // Demostración de que no construye todos los elemntos de golpe. Contruye los elemntos conforme se necesitan para la visualización
+              print("Mostrando: $index");
+              return Card(
+                margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                child: ListTile(
+                  leading: CircleAvatar(child: Text("${index + 1}")),
+                  title: Text("Elemento $index"),
+                ),
+              );
             }),
+          ),
+
+          // SliverToBoxAdapter -> es como un adaptador de enchufe, si tengo un widget normal y quiero conectarlo al mundo de slivers, lo envuelo en un SliverToBoxAdapter
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: EdgeInsetsGeometry.all(16),
+              child: Container(
+                color: Colors.red,
+                padding: EdgeInsets.all(32),
+                child: Text(
+                  "Categorías populares",
+                  style: TextStyle(
+                    fontSize: 26,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
           ),
         ],
       ),
